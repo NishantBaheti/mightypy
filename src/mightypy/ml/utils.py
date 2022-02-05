e638d51292fdb4a8d126e2356730578b3ec91d8a
Nishant Baheti <nishantbaheti.it19@gmail.com>
2022-02-05 19:55:26 +0530

    progress commit

--- src/mightypy/ml/utils.py
+++ src/mightypy/ml/utils.py
@@ -20,7 +20,7 @@ def sigmoid(val: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-val))
 
 
-def moving_window_matrix(arr: np.ndarray, window: int, shift: int = 1)-> np.ndarray:
+def moving_window_matrix(arr: np.ndarray, window: int, lag: int = 1)-> np.ndarray:
     """Create Moving Window matrix for 1D data.
 
     More details on this function.
@@ -29,31 +29,37 @@ def moving_window_matrix(arr: np.ndarray, window: int, shift: int = 1)-> np.ndar
     Args:
         arr (np.ndarray): input 1D array.
         window (int): window/ number of columns.
-        shift (int, optional): shift count for moving. Defaults to 1.
+        lag (int, optional): lag count for moving. Defaults to 1.
 
     Returns:
         np.ndarray: transformed matrix.
 
     Raises:
         AssertionError: input array shape should be 1D like (m,).
-        AssertionError: length of array should be greater than window size and shift.
+        AssertionError: length of array should be greater than window size and lag.
+
+    Example:
+        >>> a = np.random.rand(100)
+        >>> print(moving_window_matrix(a, 20, 2))
     """
 
     assert len(np.shape(arr)) == 1, 'input array shape should be 1D like (m,).'
-    assert arr.shape[0] > window and arr.shape[0] > shift, \
-        'length of array should be greater than window size and shift.'
+    size = arr.shape[0]
+    assert size > window and size > lag, \
+        'length of array should be greater than window size and lag.'
 
-    frame_width = arr.shape[0] - window + 1
+    frame_width = size - window + 1
 
-    new_frame = []
-    for i in range(0, window):
-        new_frame.append(arr[i: i+frame_width][::shift])
+    new_frame_width = int(np.ceil(frame_width/ lag))
+    new_frame = np.empty(shape=(window, new_frame_width))
+    for row in range(0, window):
+        new_frame[row] = arr[row: row+frame_width][::lag]
 
-    return np.array(new_frame).T
+    return new_frame.T
 
 
 if __name__ == "__main__":
 
-    a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    a = np.random.rand(100)
 
-    print(moving_window_matrix(a, 4, 1))
+    print(moving_window_matrix(a, 20, 2))
